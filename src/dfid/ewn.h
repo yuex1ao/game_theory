b0542f9cb559e8916a7ecb3c9127a2a11a59964d
yuex1ao <106163137+yuex1ao@users.noreply.github.com>
2023-10-11 11:33:45 +0800

    Add files via upload

---
+++ src/dfid/ewn.h
@@ -0,0 +1,48 @@
+#ifndef EWN_H
+#define EWN_H
+#include <stack>
+using namespace std;
+
+#define MAX_ROW 9
+#define MAX_COL 9
+#define MAX_PIECES 6
+#define MAX_PERIOD 18
+#define MAX_PLIES 100
+#define MAX_MOVES 16
+
+extern int ROW;
+extern int COL;
+
+class EWN {
+    int row, col; // board's row and column
+    int board[MAX_ROW * MAX_COL]; // board size
+    int pos[MAX_PIECES + 2];  // pos[0] and pos[MAX_PIECES + 1] are not used, position of each piece
+    int dice_seq[MAX_PERIOD]; // the cyclic dice sequence
+    int period; // each cycle's number quantity
+    int goal_piece; // 0 -> any piece , 1~6 -> only spicified piece can go to destination
+
+    int history[MAX_PLIES]; // previous move
+    int n_plies; // number of moves
+    stack<int> pieces;
+    stack<int> directions;
+
+public:
+    EWN();
+    void scan_board();
+    void print_board();
+    bool is_goal();
+
+    int move_gen_all(int *moves);
+    void do_move(int move);
+    void undo();
+
+    void dfid(int, int, bool&);
+    int getSize();
+    int getPiece();
+    int getDir();
+    int heuristic();
+    int heuristic2();
+    void sort_move(int *moves, int n_move);
+};
+
+#endif
